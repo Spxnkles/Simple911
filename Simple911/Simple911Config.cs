523d7ad025d699b80c928e4e50acf7ccd3d10a70
Spxnkles <108006275+Spxnkles@users.noreply.github.com>
2022-08-14 10:26:46 +0200

    Add files via upload

---
+++ Simple911/Simple911Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core.Plugins;
+using Rocket.API;
+
+namespace Simple911
+{
+    public class Simple911Config : IRocketPluginConfiguration
+    {
+        public string MessageColor;
+
+        public bool SetWaypointOnCall;
+
+
+        public void LoadDefaults()
+        {
+            MessageColor = "Red";
+
+            SetWaypointOnCall = true;
+        }
+    }
+}
